301fbfc10d78523ad0d80fca25e7ce4be2d1e72f
trev <t.vero1011@gmail.com>
2025-10-20 11:36:34 +0800

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import {FaSearch } from 'react-icons/fa';
+import {Link} from 'react-router-dom';
 
 export default function Header() {
   return (
@@ -8,9 +9,21 @@ export default function Header() {
          <span className='text-orange-900'>The Apartment</span>
          <span className='text-zinc-700'>Directories</span>
       </h1>
-      <form>
-        <input type="text" placeholder='Search....'/>
+      <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+        <input type="text" placeholder='Search....' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
+        <FaSearch className='text-slate-600' />
       </form>
+      <ul className='flex gap-4'>
+        <Link to='/'>
+        <li className='hidden sm:inline text-zinc-700 hover:font-bold text-orange-900'>Home</li>
+        </Link>
+        <Link to='/about'>
+        <li className='hidden sm:inline text-zinc-700 hover:font-bold text-orange-900'>About</li>
+        </Link>
+        <Link to='/Signin'>
+        <li className='text-zinc-700 hover:font-bold text-orange-900'>Sign in</li>
+        </Link>
+      </ul>
     </div>
     </header>
   );
